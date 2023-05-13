81e90b5f859b26b3d42d463d203aaf1f5158b6aa
Astewul <loloalex1992@gmail.com>
2023-05-13 14:57:53 +0300

    commit tasks

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -12,6 +12,8 @@ def index_range(page, page_size):
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
         return start_index, end_index
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
