a67ec70d5a13e3a9694b4fb7a390386a64632003
Astewul <loloalex1992@gmail.com>
2023-05-13 14:41:44 +0300

    commit task1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,9 +3,8 @@
 
 
 def index_range(page, page_size):
-    """
-        return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.
-    """
+    """ return a tuple of size two containing a start index
+    and an end index"""
     if page and page_size:
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
